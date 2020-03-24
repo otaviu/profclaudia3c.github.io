04d377d6824df7d17e0f13aab3996093f59e82ba
Otavio I F <60989709+otaviu@users.noreply.github.com>
2020-03-24 11:22:55 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function printImg() {
+  pwin = window.open(document.getElementById("mainImg").src,"_blank");
+  //pwin.onload = function () {window.print();}
+}
+
+function printImg2(){
+  pwin2 = window.open(document.getElementById("mainImg2").src,"_blank");
+  //pwin2.onload = function() {window.print();}
+}
